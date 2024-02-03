31ffc7215b8f083e6a5e43daf8a514ba9560d6d3
caio.santos <caioalcantarasantos3@gmail.com>
2024-02-02 22:18:58 -0300

    Adiciona o peixe e a logo do Inteli

--- peixe.js
+++ peixe.js
@@ -12,13 +12,19 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {
-    this.load.image("mar", "assets/bg_azul-escuro.png")
+var peixinho;
 
+function preload() {
+    this.load.image("mar", "assets/bg_azul-escuro.png");
+    this.load.image("logo", "assets/logo-inteli_branco.png");
+    this.load.image("peixe", "assets/peixes/baiacu.png");
 }
 
 function create() {
-    this.add.image(400, 300, "mar")
+    this.add.image(400, 300, "mar");
+    this.add.image(400, 550, "logo").setScale(0.5);
+    peixinho = this.add.image(400, 300, "peixe").setOrigin(0.5, 0.5)
+    peixinho.setFlip(true, false);
 }
 
 function update() {
