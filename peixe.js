bf829ade3d0f175d182dceaf5f3f10c49e7a9706
caio.santos <caioalcantarasantos3@gmail.com>
2024-02-07 15:46:53 -0300

    Adiciona som ambiente ao jogo

--- peixe.js
+++ peixe.js
@@ -14,12 +14,15 @@ var game = new Phaser.Game(config);
 var xPosition;
 var xPositionNew;
 var peixinho;
+var musica;
 
 function preload() {
     this.load.image("mar", "assets/bg_azul-escuro.png");
     this.load.image("logo", "assets/logo-inteli_branco.png");
     this.load.image("peixe", "assets/peixes/baiacu.png");
     this.load.image("alga", "assets/seaweed.png");
+
+    this.load.audio("musica", "assets/underwater-ambience.mp3");
 
 }
 
@@ -32,6 +35,11 @@ function create() {
     this.add.image(400, 550, "logo").setScale(0.5);
     peixinho = this.add.image(400, 300, "peixe").setOrigin(0.5, 0.5);
     peixinho.setFlip(true, false);
+
+    musica = this.sound.add("musica");
+    musica.volume = 0.5;
+    musica.loop = true;
+    musica.play();
 }
 
 function update() {
